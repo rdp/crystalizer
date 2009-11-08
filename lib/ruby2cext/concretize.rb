fca0d9ed0e483cf9e844885684376168506bb835
rdp <rogerpack2005@gmail.com>
2009-11-07 22:40:53 -0700

    avoid binding which doesn't work so hot with ruby2cextension

--- lib/ruby2cext/concretize.rb
+++ lib/ruby2cext/concretize.rb
@@ -18,10 +18,14 @@ module Ruby2CExtension
         return nil
       end
       code = @@r2r.process( processed )
-      if code.include?('&block')
+      if code.include?('&block') || code.include?('binding') 
+        # eval?
         # for now, try to avoid the tripsy case of ruby2cext not yielding arrays right yet...
         # TODO add some test cases for it in 'broken.rb' or something
         # LTODO fix it :)
+        
+        # TODO test it with rdoc...yeah. A real test sniff
+        # compare with 1.9...
         nil
       else
         code
